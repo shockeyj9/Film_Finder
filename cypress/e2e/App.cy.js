41a5a30c8fa0906db05a331566ca12a807d57a47
Jessie Shockey <shockeyjessie92@gmail.com>
2024-01-28 22:10:14 -0700

    Update cypress tests Create Search Bar loading test Create Search Bar input test Create Searched input load validations

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -1,4 +1,4 @@
-describe('Component Testing', () => {
+describe('App components functioning correctly', () => {
   beforeEach(()=>{
     cy.visit('http://localhost:3000/')
   })
@@ -8,4 +8,35 @@ describe('Component Testing', () => {
 
   it('On Page Load Shows Some Movies', () => {
     cy.get('.movie').should('have.length.greaterThan', 1)
-})})
+  })
+
+  it('Search Bar Input should be empty on page Load', ()=>{
+    cy.get('input').should('have.value', '')
+  })
+
+  it('Search Bar Input updates with text entry', ()=>{
+    const data = 'dd'
+    cy.get('input').type(data)
+    cy.get('input').should('have.value', data)
+  })
+
+  it('Search icon Loads', ()=>{
+    cy.get('img[alt="Search"]')
+  })
+
+  it('Search Icon onClick Triggers No Movies to Load', ()=>{
+    const searchIcon = 'img[alt="Search"]'
+    const data = 'd'
+    cy.get('input').type(data)
+    cy.get(searchIcon).click()
+    cy.get('.movie').should('have.length.lessThan', 1)
+  })
+
+  it('Search Icon onClick Triggers Movies to Load', ()=>{
+    const searchIcon = 'img[alt="Search"]'
+    const data = 'dog'
+    cy.get('input').type(data)
+    cy.get(searchIcon).click()
+    cy.get('.movie').should('have.length.greaterThan', 1)
+  })
+})
